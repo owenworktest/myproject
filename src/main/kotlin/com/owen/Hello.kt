678003e54c0c643850ebfb51de49e50a396d6f6f
owen <owenworktest@gmail.com>
2019-08-14 14:38:53 +0800

    Initial commit

---
+++ src/main/kotlin/com/owen/Hello.kt
@@ -0,0 +1,12 @@
+package com.owen
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin");
+    val h = Human();
+        h.hello();
+}
+class Human{
+    fun hello(){
+        println("Hello kotlin");
+    }
+}
