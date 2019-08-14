0f1a9d07a1da13a06a751d0f527063c7dac14519
owen <owenworktest@gmail.com>
2019-08-14 15:50:24 +0800

    java and kotlin var

--- src/main/kotlin/com/owen/Hello.kt
+++ src/main/kotlin/com/owen/Hello.kt
@@ -4,6 +4,8 @@ fun main(args: Array<String>) {
 //    println("Hello kotlin");
     val h = Human();
         h.hello();
+    var age: Int = 19;
+    var weight = 66.5f;
 }
 class Human{
     fun hello(){
